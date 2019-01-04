f6105ea7974b2265e1566a8033c9519ba74bdc1b
msdeepak <sadanand.deepak@gmail.com>
2019-01-03 19:12:59 -0500

    thirsty

--- hungry.py
+++ hungry.py
@@ -1,4 +1,8 @@
-print("I am hungry!")
-print("I want Pepsi!!!")
-print("I want Pizza!!!")
-print("I am also hungry dude!!")
+hungry=input("are you hungry?")
+if hungry=="yes":
+	print("I am hungry!")
+	print("I am also hungry dude!!")
+else:
+   thirsty=input("are you thirsty?")
+     if thirsty=="yes":
+	   print("drink water")
